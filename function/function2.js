be91b84ff59a6dc7e5acc96372388b26951f3b6f
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-23 10:48:29 +0530

    function

---
+++ function/function2.js
@@ -0,0 +1,28 @@
+function science(fname,per)
+{
+    console.log(fname + " your addmision confirm in science stream with per " + per);
+    
+}
+
+function commerce(fname,per){
+    console.log(fname + " your addmision confirm in commerce stream with per " + per);
+}
+
+function arts(fname,per){
+    console.log(fname + " your addmision confirm in arts stream with per " + per);
+}
+
+var percentage = 91;
+var name = "praful"
+
+
+if(percentage > 90)
+{
+    science("rahul",percentage)
+}
+else if(percentage > 70){
+    commerce(name,percentage)
+}
+else if(percentage > 50){
+    arts(name,percentage)
+}
