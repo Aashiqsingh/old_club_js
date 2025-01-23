2be037546c899d7f8b254a3a4d154b4a93e97c1c
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-01-23 14:56:14 +0530

    .

--- JsonArray/Array4.js
+++ JsonArray/Array4.js
@@ -137,7 +137,7 @@ var company = [
 // 3. Calculate the average salary of employees across all companies.
 // 4. Find the names of all employees who are married.
 // 5. Count the total number of children across all employees
-// 6.  Get the highest-paid employee and their company.
+
 // 7. List all female employees.
 // 8. Count how many employees are not married.
 // 9. Find the email addresses of all employees earning more than 30,000.
