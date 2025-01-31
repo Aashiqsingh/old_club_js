968a0521e8b8c700177313c3e5e4dad727bd2035
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-01-31 15:55:29 +0530

    ternary

---
+++ Ternary/ternary.js
@@ -0,0 +1,64 @@
+var a = parseInt(prompt("Enter first number :"))
+var b = parseInt(prompt("Enter second number :"))
+var c = parseInt(prompt("Enter third number :"))
+
+var sum = 0;
+
+if(a > 35)
+{
+    sum += a;
+}
+if(b > 35)
+{
+    sum += b;
+}
+if(c > 35){
+    sum += c;
+}
+
+console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(a > 35 && b > 35 && c > 35)
+// {
+//     console.log(a+b+c);
+    
+// }
+// else if(a > 35 && b > 35)
+// {
+//     console.log(a+b);
+// }
+// else if(a > 35 && c > 35)
+// {
+//     console.log(a+c);
+// }
+// else if(b > 35 && c > 35)
+// {
+//     console.log(b+c);
+// }
+// else if(a > 35)
+// {
+//     console.log(a);
+// }
+// else if(b > 35)
+// {
+//     console.log(b);
+// }
+// else if(c > 35){
+//     console.log(c);
+// }
+
