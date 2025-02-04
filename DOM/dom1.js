ed15bb499084fbdc59a50221923e45d44a73dc87
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-02-04 12:56:11 +0530

    dom

---
+++ DOM/dom1.js
@@ -0,0 +1,17 @@
+// console.log(document.getElementById("txt").innerHTML )
+
+// var x = document.getElementById("txt").innerHTML = "Royal technosoft pvt ltd."
+
+// console.log(x);
+
+function changeTxt(){
+
+    var x = document.getElementById("txt")
+
+    x.innerHTML = "Royal technosoft pvt ltd.";
+    x.style.color = "blue";
+    x.style.backgroundColor = "black";
+    x.style.fontSize = "34px";
+    x.style.fontWeight = "bold";
+    x.style.textShadow = "2px 2px 4px yellow";
+}
---
+++ DOM/dom2.js
@@ -0,0 +1,10 @@
+function changeHref(){
+
+
+    var link = document.getElementById("link")
+
+    link.href = "https://netflix.com";
+    link.innerHTML = "NETFLIX";
+    link.target = "_blank";
+
+}
