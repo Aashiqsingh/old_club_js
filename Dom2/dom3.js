644c3bfee628b8e02fa38311dbaa04218ac9a81c
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-02-07 15:20:16 +0530

    dom3

--- Dom2/dom3.js
+++ Dom2/dom3.js
@@ -1,9 +1,17 @@
 var a = 1;
 
+
+var colors = ["red", "green", "blue", "alpha", "orange" , "pink"];
 const move = ()=>{
 
     var mydiv = document.getElementById("mydiv");
 
-    console.log(a++);
+    var randomIndex = Math.floor(Math.random() * colors.length);
+    console.log(randomIndex);
+
+    mydiv.style.backgroundColor = colors[randomIndex]
+    
+
+    // console.log(a++);
     
 }
