94a55e797a1802c294daa49c30d8321bac8256c2
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-02-14 15:06:51 +0530

    mouseenter , mouseleave

---
+++ DOM/dom3.js
@@ -0,0 +1,12 @@
+function changeSize(){
+
+    var mydiv = document.getElementById("mydiv")
+
+    mydiv.style.height = "400px"
+    mydiv.style.width = "400px"
+    mydiv.style.transition = "2s"
+    mydiv.style.backgroundColor = "green"
+    mydiv.style.borderRadius = "50%"
+
+
+}
