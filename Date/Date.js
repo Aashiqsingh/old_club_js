3e182f99124037606e9d0ab3437dc7f1d07799a4
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-28 15:44:46 +0530

    date

---
+++ Date/Date.js
@@ -0,0 +1,57 @@
+// Date  -- class 
+
+function getDate(){
+
+    // let date = new Date();
+    // let date = new Date("01/jan/2003")
+    // let date = new Date("01/jan/2003 05:30:23")
+    // let date = new Date("2022-01-31T23:59:59");
+    
+    let date = new Date();
+
+    
+
+    // console.log(date);
+    // console.log(date.getDate());
+    // console.log(date.getDay());
+    // console.log(date.getFullYear());
+    // console.log(date.getHours());
+    // console.log(date.getMilliseconds());
+    // console.log(date.getMinutes());
+    // console.log(date.getMonth() + 1);
+    // console.log(date.getSeconds());
+    // console.log(date.getTime());
+    // console.log(date.getUTCDate());
+
+
+    // setter 
+
+    date.setDate(10);
+    date.setMonth(11);
+    date.setFullYear(2022);
+    date.setHours(23);
+    date.setMinutes(59);
+    date.setSeconds(59);
+    date.setMilliseconds(999);
+    console.log(date);
+
+    // localStorage.setItem("date",date);
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+    
+
+    
+    
+    
+
+}
+
+
+getDate()
